27fc4d088f0ce8b4a375397f55495058127ff17d
Rodney Jácome <rodneyj3214@gmail.com>
2021-08-30 21:39:49 -0500

    view special

--- posts/views.py
+++ posts/views.py
@@ -1,34 +1,40 @@
 # Create your views here.
 from datetime import datetime
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 posts = [
     {
-        'name': 'Mont Blanc',
-        'user': 'Yesica Cortes',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE'
-    }, {
-        'name': 'Mont Blanc',
-        'user': 'Yesica Cortes',
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE'
-    }, {
-        'name': 'Mont Blanc',
-        'user': 'Yesica Cortes',
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE'
-    |
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 
 def list_post(request):
-    contact = []
-    for post in posts:
-        contact.append("""
-        <p><strong>{name}</string></p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src="{picture}"></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(contact))
+
+    # return HttpResponse('<br>'.join(contact))
+    return render(request, 'feed.html', {'posts': posts})
