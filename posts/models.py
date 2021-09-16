ca73b3b8195ecedbedaf13218a35bd85eee80a5a
Rodney Jácome <rodneyj3214@gmail.com>
2021-09-15 22:43:36 -0500

    create media link to see photos is URL

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Do you can add the model or an str that contains app.Model
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} by @{}'.format(self.title, self.user.username)
--- rodneygram/urls.py
+++ rodneygram/urls.py
@@ -1,12 +1,15 @@
 """"""
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from rodneygram import views as local_views
+
 from posts import views as posts_views
+from rodneygram import views as local_views
 
 urlpatterns = [
-    path('hello-world/', local_views.hello_word),
-    path('hi/', local_views.hi),
-    path('admin/', admin.site.urls),
-    path('posts/', posts_views.list_post),
-]
+                  path('hello-world/', local_views.hello_word),
+                  path('hi/', local_views.hi),
+                  path('admin/', admin.site.urls),
+                  path('posts/', posts_views.list_post),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
