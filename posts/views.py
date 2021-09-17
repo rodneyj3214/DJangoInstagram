ca3846fb340f29316b27f49980c06394110d3657
Rodney Jácome <rodneyj3214@gmail.com>
2021-09-16 19:06:16 -0500

    Template and static files

--- posts/views.py
+++ posts/views.py
@@ -37,4 +37,4 @@ posts = [
 def list_post(request):
 
     # return HttpResponse('<br>'.join(contact))
-    return render(request, 'feed.html', {'posts': posts})
+    return render(request, 'posts/feed.html', {'posts': posts})
