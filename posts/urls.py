78a6a809c2078db0ae3a0c70d1f82231c9b1a1ef
Rodney Jácome <rodneyj3214@gmail.com>
2021-09-26 21:03:45 -0500

    good url for apps

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from posts import views
+
+urlpatterns = [
+    path(route='', view=views.list_post, name='feed'),
+    path(route='posts/new/', view=views.create_post, name='create_post'),
+]
--- rodneygram/urls.py
+++ rodneygram/urls.py
@@ -2,20 +2,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from posts import views as posts_views
 from rodneygram import views as local_views
 from users import views as users_views
 
 urlpatterns = [
-                  path('hello-world/', local_views.hello_word, name='hello_world'),
-                  path('hi/', local_views.hi),
-                  path('admin/', admin.site.urls),
-                  path('', posts_views.list_post, name='feed'),
-                  path('posts/new/', posts_views.create_post, name='create_post'),
-                  path('users/login/', users_views.login_view, name='login'),
-                  path('users/logout/', users_views.logout_view, name='logout'),
-                  path('users/signup/', users_views.signup_view, name='signup'),
-                  path('users/me/profile/', users_views.update_profile, name='update_profile'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  path('admin/', admin.site.urls),
+
+  path('', include(('posts.urls', 'posts'), namespace='posts')),
+  path('users/', include(('users.urls', 'users'), namespace='users')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
